43686baf44e5f5ee11282a38540f14e58df8aec7
Karol115 <karol20080518@gmail.com>
2025-10-20 22:05:15 +0200

    Add project files.

---
+++ WoT_[1-DWP]/Program.cs
@@ -0,0 +1,59 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Microsoft.Extensions.Logging;
+using WoT__1_DWP_.config;
+
+namespace WoT__1_DWP_
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            var config = new JsonReader();
+            await config.ReadJsonConfig();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = config.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+            Client = new DiscordClient(discordConfig);
+
+            Client.Ready += Client_Ready;
+
+
+            Client.GuildMemberUpdated += async (s, e) =>
+            {
+                var addedRoles = e.Guild.Roles.Values.Where(r => !e.RolesBefore.Contains(r) && e.RolesAfter.Contains(r));
+                var removedRoles = e.Guild.Roles.Values.Where(r => e.RolesBefore.Contains(r) && !e.RolesAfter.Contains(r));
+
+                var channel = await Client.GetChannelAsync(config.channel_id);
+
+                foreach (var role in addedRoles)
+                    await channel.SendMessageAsync($"{e.Member.DisplayName} dostał rangę: {role.Name}");
+
+                foreach (var role in removedRoles)
+                    await channel.SendMessageAsync($"{e.Member.DisplayName} stracił rangę: {role.Name}");
+
+            };
+
+            /*  Remove warning  TO DO  */
+            
+
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ WoT_[1-DWP]/config/JSONReader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WoT__1_DWP_.config
+{
+    internal class JsonReader
+    {
+        public string token {  get; set; }
+        public string prefix { get; set; }
+        public ulong channel_id { get; set; }
+
+
+        public async Task ReadJsonConfig()
+        {
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+
+                JSONConfig config = JsonSerializer.Deserialize<JSONConfig>(json);
+
+                this.token = config.token;
+                this.prefix = config.prefix;
+                this.channel_id = config.channel_id;
+            }
+        }
+    }
+
+    internal sealed class JSONConfig
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+        public ulong channel_id { get; set; }
+    }
+}
